b1b8c89363b4bd9039d4b73fd95c5761dc65eb23
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2024-02-11 18:15:39 -0500

    next-auth successfully setup

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { getServerSession, Session } from 'next-auth';
+import { options } from '../api/auth/[...nextauth]/options';
+import Link from "next/link";
 
-const Navbar = () => {
+const Navbar: React.FC = async () => {
+
+
+  const session = await getServerSession(options);
+  console.log("session is ", session);
+
   return (
-    <div className='w-full h-16 bg-blue-500 flex justify-start  items-center' > 
-    <h1 className='ml-4 font-semibold text-white text-xl cursor-pointer rounded-md p-2 hover:text-neutral-200' >Login</h1>
-    <h1 className='ml-4 font-semibold text-white text-xl cursor-pointer' > Please login/signup to continue </h1>
+    <div className='w-full h-16 bg-blue-500 flex justify-start items-center'>
+      {session && <Link href="/api/auth/signout?callbackUrl=/" className='ml-4 font-semibold text-white text-xl cursor-pointer rounded-md p-2 hover:text-neutral-200'>Logout</Link>}
+      {!session && <Link href="/api/auth/signin" className='ml-4 font-semibold text-white text-xl cursor-pointer rounded-md p-2 hover:text-neutral-200'>Login</Link>}
+      <h1 className='ml-4 font-semibold text-white text-xl cursor-pointer'> Please login/signup to continue </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
