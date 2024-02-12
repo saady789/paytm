a438ef87561cd7298da2b989dad54a1158ba3fd8
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2024-02-11 23:26:01 -0500

    login and signup created

--- app/Login/page.tsx
+++ app/Login/page.tsx
@@ -1,9 +1,58 @@
+"use client";
 import React from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+
+interface FormValues {
+    email: string;
+    password: string;
 }
 
-export default page
+const page: React.FC = () => {
+
+    const {
+        watch,
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>();
+
+
+
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+
+        console.log(data)
+    }
+
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h2 className="text-2xl mb-4">Sign Up</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
+                        <input type="email" id="email" className="w-full border rounded px-3 py-2" {...register("email", { required: true })} />
+                        {errors.email && <span className="text-red-500 text-sm">Email is required</span>}
+                        {!errors.email && <span className="text-white text-sm">a</span>}
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="w-full border rounded px-3 py-2" {...register("password", { required: true, minLength: 4 })} />
+                        {errors.password && <span className="text-red-500 text-sm">Password is required (min length 4)</span>}
+                        {!errors.password && <span className="text-white text-sm">a</span>}
+                    </div>
+
+
+                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Log In</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default page;
--- app/Signup/page.tsx
+++ app/Signup/page.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+"use client"
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+interface FormValues {
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
 }
 
-export default page
+const page: React.FC = () => {
+
+    const {
+        watch,
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<FormValues>();
+
+    const password = React.useRef({});
+    password.current = watch("password");
+
+
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+
+        console.log(data)
+    }
+
+
+    return (
+        <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+            <div className="bg-white p-8 rounded shadow-md w-96">
+                <h2 className="text-2xl mb-4">Sign Up</h2>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="name">Name</label>
+                        <input type="text"  id="name" className="w-full border rounded px-3 py-2" {...register("name", { required: true })} />
+                        {errors.name && <span className="text-red-500 text-sm">Name is required</span>}
+                        {!errors.name && <span className="text-white text-sm">a</span>}
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="email">Email</label>
+                        <input type="email" id="email" className="w-full border rounded px-3 py-2" {...register("email", { required: true })} />
+                        {errors.email && <span className="text-red-500 text-sm">Email is required</span>}
+                        {!errors.email && <span className="text-white text-sm">a</span>}
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="password">Password</label>
+                        <input type="password" id="password" className="w-full border rounded px-3 py-2" {...register("password", { required: true, minLength:4 })} />
+                        {errors.password && <span className="text-red-500 text-sm">Password is required (min length 4)</span>}
+                        {!errors.password && <span className="text-white text-sm">a</span>}
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-bold mb-2" htmlFor="confirmPassword">Confirm Password</label>
+                        <input type="password" id="confirmPassword" className="w-full border rounded px-3 py-2" {...register("confirmPassword", { validate: value => value === password.current || "The passwords do not match" })} />
+                        {errors.confirmPassword && <span className="text-red-500 text-sm">Password do not match</span>}
+                        {!errors.confirmPassword && <span className="text-white text-sm">a</span>}
+                    </div>
+
+                    <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Sign Up</button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default page;
